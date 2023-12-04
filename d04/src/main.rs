f7441fe743b0dba5c12cd2e55c3cd9bb3a6aca2b
Ben Nagy <nagy.benb@gmail.com>
2023-12-04 20:25:45 +0000

    feat: finish day 4

--- d04/src/main.rs
+++ d04/src/main.rs
@@ -10,14 +10,39 @@ use nom::{
 fn main() {
     let input = include_str!("../input.txt");
     println!("Part 1: {}", scratchcard_points(input));
+    println!("Part 1: {}", part2(input));
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct Scratchcard {
+    card_numbers: Vec<u32>,
+}
+
+impl Scratchcard {
+    fn parse(input: &str) -> IResult<&str, Scratchcard> {
+        let (input, _) = tag("Card")(input)?;
+        let (input, _) = space1(input)?;
+        let (input, _) = digit1(input)?;
+        let (input, _) = tag(": ")(input)?;
+        let (input, numbers) = separated_list1(
+            space1,
+            map_res(preceded(space0, digit1), |s: &str| s.parse::<u32>()),
+        )(input)?;
+        Ok((
+            input,
+            Scratchcard {
+                card_numbers: numbers,
+            },
+        ))
+    }
 }
 
 fn scratchcard_points(input: &str) -> u32 {
-    let (_, cards) = separated_list1(tag("\n"), parse_scratchcard)(input).unwrap();
-    cards.iter().fold(0, |total, (card_numbers, my_numbers)| {
+    let (_, cards) = parse_scratchcards(input).unwrap();
+    cards.iter().fold(0, |total, (scratchcard, my_numbers)| {
         let out = total
             + my_numbers.iter().fold(0, |acc, my_number| {
-                if card_numbers.contains(my_number) {
+                if scratchcard.card_numbers.contains(my_number) {
                     if acc == 0 {
                         1
                     } else {
@@ -31,20 +56,13 @@ fn scratchcard_points(input: &str) -> u32 {
     })
 }
 
-fn parse_scratchcards(input: &str) -> IResult<&str, Vec<(Vec<u32>, Vec<u32>)>> {
+fn parse_scratchcards(input: &str) -> IResult<&str, Vec<(Scratchcard, Vec<u32>)>> {
     separated_list1(tag("\n"), parse_scratchcard)(input)
 }
 
-fn parse_scratchcard(input: &str) -> IResult<&str, (Vec<u32>, Vec<u32>)> {
-    let (input, _) = tag("Card")(input)?;
-    let (input, _) = space1(input)?;
-    let (input, _) = digit1(input)?;
-    let (input, _) = tag(": ")(input)?;
+fn parse_scratchcard(input: &str) -> IResult<&str, (Scratchcard, Vec<u32>)> {
     let (input, (card_numbers, my_numbers)) = separated_pair(
-        separated_list1(
-            space1,
-            map_res(preceded(space0, digit1), |s: &str| s.parse::<u32>()),
-        ),
+        Scratchcard::parse,
         tag(" | "),
         separated_list1(
             space1,
@@ -52,6 +70,32 @@ fn parse_scratchcard(input: &str) -> IResult<&str, (Vec<u32>, Vec<u32>)> {
         ),
     )(input)?;
     Ok((input, (card_numbers, my_numbers)))
+}
+
+fn scratchcard_matches(sc: &Scratchcard, my_numbers: &[u32]) -> usize {
+    sc.card_numbers
+        .iter()
+        .filter(|n| my_numbers.contains(n))
+        .count()
+}
+
+fn part2(input: &str) -> u32 {
+    let (_, cards) = parse_scratchcards(input).unwrap();
+
+    // card_instances is a vector of the number of times each card appears in the scratchcards
+    // we initialise it to 1s, as each card appears once to begin with
+    // then we iterate through each card, and for each number in the card, we check how many
+    // matches we have. Then we increment n_matches positions forward by the current card_instances
+    let mut card_instances = vec![1; cards.len()];
+    for i in 0..card_instances.len() {
+        let sc = &cards[i].0;
+        let my_numbers = &cards[i].1;
+        let n_matches = scratchcard_matches(sc, my_numbers);
+        for j in 1..=n_matches {
+            card_instances[i + j] += card_instances[i];
+        }
+    }
+    card_instances.into_iter().sum()
 }
 
 #[cfg(test)]
@@ -76,26 +120,56 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#;
             Ok((
                 "",
                 vec![
-                    (vec![41, 48, 83, 86, 17], vec![83, 86, 6, 31, 17, 9, 48, 53]),
                     (
-                        vec![13, 32, 20, 16, 61],
+                        Scratchcard {
+                            card_numbers: vec![41, 48, 83, 86, 17]
+                        },
+                        vec![83, 86, 6, 31, 17, 9, 48, 53]
+                    ),
+                    (
+                        Scratchcard {
+                            card_numbers: vec![13, 32, 20, 16, 61]
+                        },
                         vec![61, 30, 68, 82, 17, 32, 24, 19]
                     ),
-                    (vec![1, 21, 53, 59, 44], vec![69, 82, 63, 72, 16, 21, 14, 1]),
                     (
-                        vec![41, 92, 73, 84, 69],
+                        Scratchcard {
+                            card_numbers: vec![1, 21, 53, 59, 44]
+                        },
+                        vec![69, 82, 63, 72, 16, 21, 14, 1]
+                    ),
+                    (
+                        Scratchcard {
+                            card_numbers: vec![41, 92, 73, 84, 69]
+                        },
                         vec![59, 84, 76, 51, 58, 5, 54, 83]
                     ),
                     (
-                        vec![87, 83, 26, 28, 32],
+                        Scratchcard {
+                            card_numbers: vec![87, 83, 26, 28, 32]
+                        },
                         vec![88, 30, 70, 12, 93, 22, 82, 36]
                     ),
                     (
-                        vec![31, 18, 13, 56, 72],
+                        Scratchcard {
+                            card_numbers: vec![31, 18, 13, 56, 72]
+                        },
                         vec![74, 77, 10, 23, 35, 67, 36, 11]
                     )
                 ]
             ))
         );
+    }
+
+    #[test]
+    fn test_scratchcard_matches() {
+        let sc = Scratchcard {
+            card_numbers: vec![41, 48, 83, 86, 17],
+        };
+        assert_eq!(scratchcard_matches(&sc, &[83, 86, 6, 31, 17, 9, 48, 53]), 4);
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(EXAMPLE1), 30);
     }
 }
